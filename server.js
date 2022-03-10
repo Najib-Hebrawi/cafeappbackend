6af480fcc99b72e9e5cb383943fa459504597a49
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-03-10 20:32:21 +0100

    add express as an option.

--- server.js
+++ server.js
@@ -14,3 +14,26 @@ server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+
+
+
+
+
+/*
+ const express= require('express');
+
+const app = express();
+
+
+const hostname = 'localhost';
+const port = 3000;
+
+app.get('/', (req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Hello From serverrrrrrrr')
+})
+app.listen(port,hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
+ */
