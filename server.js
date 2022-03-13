4517a4eefba4e3325f0b4b0768d5e75950100846
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-03-13 09:22:34 +0100

    adding env done .

--- server.js
+++ server.js
@@ -24,21 +24,23 @@ server.listen(port, hostname, () => {
  // config method is the one thats going off and read our enviromental variables file and save all those variables.
  // so when this script runs it will have access to all of those through somethings called (process.env).
  // and we need to create a file saving the variables.
+ // we can set an enviroment variable by exexuting (export command) for mac, and (set command) for windows.
  require('dotenv').config();
+ //{path: path/filename}
  
 
 
  // here we define a hostname and port, in our case they should be our server.
-const hostname = 'localhost';
-const port = 3000;
+const hostname = process.env.HOST;
+const port = process.env.PORT;
 
 
 // here we define a route.
 // here indside the route we should have a list of ( products in our project (crepe cafe)) from  database and return them.
 app.get('/', (req,res) => {
-    console.log('thanks for the request');
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.send('Hello From serverrrrrrrr')
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Hello From server');
 });
 
 
