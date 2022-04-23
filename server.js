9627ddb99fd09033eff6fb4b7f411150deb59376
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-04-23 18:28:35 +0200

    add routes

--- server.js
+++ server.js
@@ -11,4 +11,16 @@ res.render('index.ejs')
 });
 
 
+
+app.get('/login', (req, res) =>{
+    res.render('login.ejs')
+
+});
+
+app.get('/register', (req, res) =>{
+    res.render('register.ejs')
+
+});
+
+
 app.listen(3000);
