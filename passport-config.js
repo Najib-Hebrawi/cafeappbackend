50dd4dfc459e23d46b3e8e292c3b7128b29976a9
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-04-24 01:16:09 +0200

    write code for passport-config.js

---
+++ passport-config.js
@@ -0,0 +1,38 @@
+// in order to store and persist our user across different pages. (express-session)
+// in order to display messages for if we fail to login.(express-flash)
+/*
+Passport is authentication middleware for Node.js. Extremely flexible and modular, Passport can be unobtrusively dropped in to any Express-based web application. 
+A comprehensive set of strategies support authentication using a username and password, Facebook, Twitter, and more.
+*/
+
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
+    const user = getUserByEmail(email)
+    if (user == null) {
+      return done(null, false, { message: 'No user with that email' })
+    }
+
+    try {
+        // compare password to user.password
+      if (await bcrypt.compare(password, user.password)) {
+          // return the user
+        return done(null, user)
+      } else {
+        return done(null, false, { message: 'Password incorrect' })
+      }
+    } catch (e) {
+      return done(e)
+    }
+  }
+
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
+  passport.serializeUser((user, done) => done(null, user.id))
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+  })
+}
+
+module.exports = initialize
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const brcypt = require('bcrypt');
+const passport = require('passport');
+const initializePassport = require('./passport-config');
+initializePassport(passport);
+
 
 
 
