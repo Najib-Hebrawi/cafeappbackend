ba2a8d80197990ff7750988db6938ca376f5bd9e
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-04-23 21:22:52 +0200

    ADD. app.use(express.urlencoded({extended: false}));

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 
 app.set('view-engine', 'ejs');
+// that telling our application that what we want to do is take these forms from
+// our email and password and we want to be able to access them inside of our request variable inside of our post method 
+app.use(express.urlencoded({extended: false}));
+
 
 
 app.get('/' , (req, res) =>{
@@ -12,20 +16,32 @@ res.render('index.ejs')
 
 
 
+
+
+
 app.get('/login', (req, res) =>{
     res.render('login.ejs')
 
 });
+app.post('/login', (req,res) =>{
+
+});
+
+
+
+
+
 
 app.get('/register', (req, res) =>{
     res.render('register.ejs')
 
 });
-
-
 app.post('/register', (req,res) =>{
 
 });
+
+
+
 
 
 
