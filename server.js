6a8f59eba085b94c1d3990a4581ecc087acd2520
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-04-23 21:30:05 +0200

    add database as local variable

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express();
 
 
 
+// we store our users in a local variable inside our server. (is something you would never want to do in production, but it is easy for os)
+const users =[]
+
 app.set('view-engine', 'ejs');
+
 // that telling our application that what we want to do is take these forms from
-// our email and password and we want to be able to access them inside of our request variable inside of our post method 
+// our email and password and we want to be able to access them inside of our request variable inside of our post method.
 app.use(express.urlencoded({extended: false}));
 
 
@@ -37,6 +41,7 @@ app.get('/register', (req, res) =>{
 
 });
 app.post('/register', (req,res) =>{
+    req.body.email
 
 });
 
