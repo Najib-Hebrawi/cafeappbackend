3554a6cf50b74b512872d0fa662324b1fdf72a2d
Dragon <s195462@student.dtu.dk>
2022-04-27 14:48:58 +0200

    created POST PUT DELETE and GET request

--- controller/OrderProductController.js
+++ controller/OrderProductController.js
@@ -1,5 +1,7 @@
 const Order = require('../model/OrderProduct')
 
+
+//POST
 createOrder = (req,res) =>{
     const body =req.body
     if (!body){
@@ -34,6 +36,97 @@ createOrder = (req,res) =>{
         })
 }
 
+//GET
+getOrderById = async (req, res) => {
+    await Order.findOne({_id:req.params.id},(err,order) => {
+        if (err){
+            return res.status(400).json({ success:false,error:err})
+        }
+        if (!order) {
+            return res
+                .status(404)
+                .json({ success: false, error: `order not found` })
+        }
+        return res.status(200).json({ success: true, data: order })
+    }).catch(err => console.log(err))
+}
+
+
+//DELETE
+deleteOrder = async (req, res) => {
+    await Order.findOneAndDelete({ _id: req.params.id }, (err, order) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+
+        if (!order) {
+            return res
+                .status(404)
+                .json({ success: false, error: `order not found` })
+        }
+
+        return res.status(200).json({ success: true, data: order })
+    }).catch(err => console.log(err))
+}
+
+
+//PUT
+updateOrder = async (req, res) => {
+    const body = req.body
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update',
+        })
+    }
+
+    Order.findOne({ _id: req.params.id }, (err, order) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Movie not found!',
+            })
+        }
+        order.username = body.username
+        order.phoneNumber = body.phoneNumber
+        order.userComment = body.userComment
+        order
+            .save()
+            .then(() => {
+                return res.status(200).json({
+                    success: true,
+                    id: order._id,
+                    message: 'Order updated!',
+                })
+            })
+            .catch(error => {
+                return res.status(404).json({
+                    error,
+                    message: 'Order not updated!',
+                })
+            })
+    })
+}
+
+getOrders= async (req, res) => {
+    await Order.find({}, (err, orders) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!orders.length) {
+            return res
+                .status(404)
+                .json({ success: false, error: `orders not found` })
+        }
+        return res.status(200).json({ success: true, data: orders })
+    }).catch(err => console.log(err))
+}
+
 module.exports = {
-    createOrder
+    createOrder,
+    getOrderById,
+    deleteOrder,
+    updateOrder,
+    getOrders
 }
--- route/OrderRouter.js
+++ route/OrderRouter.js
@@ -3,5 +3,8 @@ const OrderController = require('../controller/OrderProductController')
 
 const router = express.Router()
 router.post('/orders',OrderController.createOrder)
-
+router.put('/orders/:id', OrderController.updateOrder)
+router.delete('/orders/:id', OrderController.deleteOrder)
+router.get('/orders/:id', OrderController.getOrderById)
+router.get('/orders', OrderController.getOrders)
 module.exports = router
