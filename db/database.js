3243f272521fd7804d29aabd0e3472199a7f63d3
DragonHeart <s195462@student.dtu.dk>
2022-04-27 16:34:16 +0200

    added db name

--- db/database.js
+++ db/database.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL, {
+    dbName: "DB",
     useNewUrlParser: true,
     useUnifiedTopology: true,
     family: 4
